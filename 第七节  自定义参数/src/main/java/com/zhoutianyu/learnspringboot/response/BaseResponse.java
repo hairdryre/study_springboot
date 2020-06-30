351f54e8c69d5520132f72516ce071ccd16a8dfd
tyzhou <1137214912@qq.com>
2020-06-30 19:49:23 +0800

    add 7

--- 第七节  自定义参数/src/main/java/com/zhoutianyu/learnspringboot/response/BaseResponse.java
+++ 第七节  自定义参数/src/main/java/com/zhoutianyu/learnspringboot/response/BaseResponse.java
@@ -11,23 +11,6 @@ public class BaseResponse<T> {
 
     private T data;
 
-    public BaseResponse(int code, String message) {
-        this.code = code;
-        this.message = message;
-        this.data = null;
-    }
-
-    public BaseResponse(int code, String message, T data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
-
-    public BaseResponse(StatusCodeEnum statusCodeEnum) {
-        this.code = statusCodeEnum.getCode();
-        this.message = statusCodeEnum.getMessage();
-        this.data = null;
-    }
 
     public BaseResponse(StatusCodeEnum statusCodeEnum, T data) {
         this.code = statusCodeEnum.getCode();
